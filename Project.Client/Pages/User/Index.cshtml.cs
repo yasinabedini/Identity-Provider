644aed78176cdc0e4e065816d2686b62f33d5cac
yasin abedini <yasinabedini1384@gmail.com>
2024-03-06 21:08:08 +0330

    feat - config Client

--- Project.Client/Pages/User/Index.cshtml.cs
+++ Project.Client/Pages/User/Index.cshtml.cs
@@ -1,24 +1,35 @@
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 using Project.Api.Models;
 
 namespace Project.Client.Pages.Shared.User
-{     
+{
     public class IndexModel : PageModel
     {
-        private readonly HttpClient _userService;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public IndexModel(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
 
         public List<Project.Api.Models.User> Users { get; set; }
 
-        public IndexModel(IHttpClientFactory factory)
-        {
-            _userService = factory.CreateClient("UserService");
-        }
 
         public async Task OnGet()
-        {   
-            Users = JsonConvert.DeserializeObject<List<Project.Api.Models.User>>(await _userService.GetStringAsync("api/user/GetAll"));
+        {
+            HttpClient _userService = _clientFactory.CreateClient("UserService");
+
+            string token = await HttpContext.GetTokenAsync("access_token");
+            _userService.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            
+            string userString = await _userService.GetStringAsync("api/user/GetAll");
+
+
+            Users = JsonConvert.DeserializeObject<List<Project.Api.Models.User>>(userString);
         }
     }
 }
--- Project.Client/Program.cs
+++ Project.Client/Program.cs
@@ -1,9 +1,46 @@
+using System.IdentityModel.Tokens.Jwt;
+using IdentityModel;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+#region Http Clients
 builder.Services.AddHttpClient("UserService", client =>
 {
-    client.BaseAddress = new Uri("http://localhost:5053/");
+client.BaseAddress = new Uri("https://localhost:7024/");
 });
+
+builder.Services.AddHttpClient("oAtuh", c =>
+{
+    c.BaseAddress = new Uri("https://localhost:5001");
+}); 
+#endregion
+
+
+#region Authentication
+
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+builder.Services.AddAuthentication(c =>
+{
+c.DefaultScheme = "Cookies";
+c.DefaultChallengeScheme = "oidc";
+}).AddCookie("Cookies")
+.AddOpenIdConnect("oidc", c =>
+{
+c.Authority = "https://localhost:5001";
+c.ClientId = "Project.Client";
+c.ClientSecret = "Project.Client";
+c.ResponseType = "code";
+c.Scope.Clear();
+c.Scope.Add("openid");
+c.Scope.Add("profile");
+c.Scope.Add("Project.Api");
+c.Scope.Add("offline_access");
+c.GetClaimsFromUserInfoEndpoint = true;
+c.SaveTokens = true;
+}); 
+#endregion
 
 
 // Add services to the container.
@@ -24,8 +61,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapRazorPages().RequireAuthorization();
 
 app.Run();
